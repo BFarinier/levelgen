dbb3fca48faa2f6dbffd7b9a90c07846ef01c5ef
Benjamin <benjamin.farinier@ens-lyon.fr>
2015-12-21 13:45:13 +0100

    minor improvements

--- main.ml
+++ main.ml
@@ -2,6 +2,7 @@ open Gg
 open Graphics
 open Level
 
+
 let rec sleep t =
   if t > 0. then
     let now = Unix.gettimeofday () in
@@ -13,31 +14,39 @@ let ( >> ) v f =
   let x,y = V2.to_tuple v in
   f (truncate x) (truncate y)
 
-let print_path color =
+
+let print_segment color =
   set_color color;
-  List.iter
-    (fun (v1,v2) ->
-       v1 >> moveto;
-       v2 >> lineto)
+  (fun (v1,v2) ->
+     v1 >> moveto;
+     v2 >> lineto)
+
+let print_path color =
+  List.iter (print_segment color)
+
+
+let print_triangle color =
+  set_color color;
+  (fun (v1,v2,v3) ->
+     v1 >> moveto;
+     v2 >> lineto;
+     v3 >> lineto;
+     v1 >> lineto)
 
 let print_level color =
-  set_color color;
-  iter
-    (fun (v1,v2,v3) ->
-       v1 >> moveto;
-       v2 >> lineto;
-       v3 >> lineto;
-       v1 >> lineto)
+  iter (print_triangle color)
 
 let () =
   Random.self_init ();
   open_graph " 1024x1024";
   auto_synchronize false;
 
-  let biased = biased_points ~x:1024. ~y:1024. ~nbr:1024 in
-  let random = random_points ~x:1024. ~y:1024. ~nbr:1024 in
-  let path = spanning_tree biased in
-  let level = create random in
+  let path =
+    biased_points ~x:1024. ~y:1024. ~nbr:1024
+    |> spanning_tree in
+  let level =
+    random_points ~x:1024. ~y:1024. ~nbr:1024
+    |> create in
   print_level blue level;
   print_path red path;
   synchronize ();
@@ -45,18 +54,21 @@ let () =
   let level =
     List.fold_left
       (fun lvl v ->
-         print_level green lvl;
          let lvl =
-           List.fold_left (fun lvl tr -> remove_triangle tr lvl)
+           List.fold_left
+             (fun lvl tr ->
+                print_triangle green tr;
+                remove_triangle tr lvl)
              lvl (triangles_intersecting v lvl)
          in
          print_level blue lvl;
          print_path red path;
          synchronize ();
-         sleep 0.01;
+         sleep 0.001;
          lvl)
       level path
-  in print_level blue level;
+  in
+  print_level blue level;
   synchronize ();
-  sleep 1.;
+  let _ = wait_next_event [Button_down; Button_up; Key_pressed] in
   exit 0
